185f3bb909ee66bc73bf0d059b863c90ea8343f7
Jennifer Hanson <jenniferhanson.sd@gmail.com>
2023-04-03 17:05:50 -0700

    Added CSS Styling

--- src/Login.js
+++ src/Login.js
@@ -13,33 +13,42 @@ function Login() {
   function handleSubmit(event) {
     event.preventDefault();
   }
-
+ const styles = {
+        border: '3px solid hotpink',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+     }
   return (
+   
     <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      <div style={styles}>
+        <h3>Login</h3>
+      </div>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+                autoFocus
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
 
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
-        </Form.Group>
-        <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+            </Form.Group>
+            <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
+            Login
+            </Button>
+        </Form>
     </div>
   );
 
